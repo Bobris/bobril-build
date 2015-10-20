5b92d9321d63d0062ada7849ddd8e96a48913ec5
Boris Letocha <boris.letocha@gmail.com>
2015-10-21 01:07:22 +0200

    Add polyfill for Object.assign for old nodejs.

--- src/bundler.ts
+++ src/bundler.ts
@@ -4,6 +4,39 @@ const path = pathPlatformDependent.posix; // This works everythere, just use for
 import * as pathUtils from './pathUtils';
 import * as uglify from 'uglifyjs';
 
+if (!Object.assign) {
+    Object.defineProperty(Object, 'assign', {
+        enumerable: false,
+        configurable: true,
+        writable: true,
+        value: function (target) {
+            'use strict';
+            if (target === undefined || target === null) {
+                throw new TypeError('Cannot convert first argument to object');
+            }
+
+            var to = Object(target);
+            for (var i = 1; i < arguments.length; i++) {
+                var nextSource = arguments[i];
+                if (nextSource === undefined || nextSource === null) {
+                    continue;
+                }
+                nextSource = Object(nextSource);
+
+                var keysArray = Object.keys(nextSource);
+                for (var nextIndex = 0, len = keysArray.length; nextIndex < len; nextIndex++) {
+                    var nextKey = keysArray[nextIndex];
+                    var desc = Object.getOwnPropertyDescriptor(nextSource, nextKey);
+                    if (desc !== undefined && desc.enumerable) {
+                        to[nextKey] = nextSource[nextKey];
+                    }
+                }
+            }
+            return to;
+        }
+    });
+}
+
 export interface IFileForBundle {
     name: string;
     astTime: number;
