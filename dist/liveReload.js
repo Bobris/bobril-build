413d38710dd633bec2d7161cd1f619e9ff3449a4
Boris Letocha <boris.letocha@gmail.com>
2017-02-08 11:50:25 +0100

    ES5 for PhantomJs

--- dist/liveReload.js
+++ dist/liveReload.js
@@ -1,18 +1,19 @@
 function liveReloadWaiter() {
+    var _this = this;
     var xhr = new window.XMLHttpRequest();
     xhr.open("GET", "./bb/api/livereload/##Idx##", true);
-    xhr.onabort = () => {
-        this.close();
+    xhr.onabort = function () {
+        _this.close();
         liveReloadWaiter();
     };
-    xhr.onreadystatechange = () => {
+    xhr.onreadystatechange = function () {
         if (xhr.readyState === 4) {
             if (xhr.status < 200 || xhr.status >= 300) {
-                this.close();
+                _this.close();
                 liveReloadWaiter();
             }
             else {
-                this.close();
+                _this.close();
                 window.location.reload(true);
                 return;
             }
